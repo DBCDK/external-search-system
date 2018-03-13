1d784f1c900667696aa4cf6cf738ba1f0d563644
Mike Andersen <mran@dbc.dk>
2018-03-13 10:04:06 +0100

    Wiremock setup og start på acceptance tests

---
+++ service/src/test/java/dk/dbc/ess/service/EssServiceIT.java
@@ -0,0 +1,106 @@
+package dk.dbc.ess.service;
+
+import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import io.dropwizard.client.JerseyClientBuilder;
+import io.dropwizard.testing.ConfigOverride;
+import io.dropwizard.testing.ResourceHelpers;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import org.glassfish.jersey.client.ClientProperties;
+import org.glassfish.jersey.client.JerseyClient;
+import org.junit.*;
+
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.core.Response;
+
+import java.util.UUID;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static junit.framework.TestCase.assertEquals;
+
+
+public class EssServiceIT {
+    private static final String CONFIG_PATH = ResourceHelpers.resourceFilePath("config_test.yaml");
+    private EssConfiguration conf;
+    private Client client;
+    private EssService essService;
+
+
+    private final  String essHttpPort =  System.getProperty("ess.http.port", "8010");
+    private final  String essHttpsPort =  System.getProperty("ess.https.port", "8011");
+    private final static String wiremockHttpPort =  System.getProperty("wiremock.test-http.port", "8020");
+    private final static String wiremockHttpsPort =  System.getProperty("wiremock.test-https.port", "8021");
+
+    @ClassRule
+    public final static WireMockClassRule wireMockRule = new WireMockClassRule(wireMockConfig()
+            .port(Integer.parseInt(wiremockHttpPort))
+            .httpsPort(Integer.parseInt(wiremockHttpsPort))
+    );
+
+    @Rule
+    public WireMockClassRule instanceRule = wireMockRule;
+
+    @ClassRule
+    public final static DropwizardAppRule<EssConfiguration> dropWizzardRule = new DropwizardAppRule<>(EssApplication.class, CONFIG_PATH,
+            ConfigOverride.config("settings.metaProxyUrl", "http://localhost:" + wiremockHttpPort + "/"),
+            ConfigOverride.config("settings.openFormatUrl", "http://localhost:" + wiremockHttpPort + "/"));
+
+
+
+    @Before
+    public void setUp() {
+        conf = dropWizzardRule.getConfiguration();
+        client = new JerseyClientBuilder(dropWizzardRule.getEnvironment()).build(UUID.randomUUID().toString() );
+
+        // Timeouts on JerseyClients is neccesary for WireMock / Dropwizzard don't run
+        // into timeouts due to startup complications.
+        client.property(ClientProperties.CONNECT_TIMEOUT, 1000);
+        client.property(ClientProperties.READ_TIMEOUT,    1000);
+
+        essService = new EssService(conf.getSettings(), dropWizzardRule.getEnvironment().metrics(), client );
+    }
+
+    @Test
+    public void foundTest() throws Exception {
+        /*stubFor(get(urlEqualTo("/api/?base=bibsys&query=horse&start=&rows=1&format=netpunkt_standard&trackingId="))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "text/xml")
+                        .withBody("<response>Some content</response>")));*/
+
+        Response result = essService.requestSru("bibsys", "horse", 1, 1);
+        assertEquals(200, result.getStatus());
+    }
+
+    @Test
+    public void notfoundTest() throws Exception {
+        stubFor(get(urlMatching(".*dog.*"))
+                .willReturn(aResponse()
+                        .withStatus(404)));
+
+        Response result = essService.requestSru("bibsys", "dog", 1, 1);
+        assertEquals(404, result.getStatus());
+    }
+
+    @Test
+    public void fullTest() throws Exception {
+        Response response = client.target(
+                String.format("http://localhost:%d/api/?base=bibsys&query=horse&start=&rows=1&format=netpunkt_standard&trackingId=test200", dropWizzardRule.getLocalPort()))
+                .request()
+                .get();
+        System.out.println( "fulltest: wiremockHttpPort = " + wiremockHttpPort );
+        assertEquals(200, response.getStatus());
+    }
+
+
+    @Test
+    public void fullFailTest() throws Exception {
+        Response response = client.target(
+                String.format("http://localhost:%d/api/?base=bibsys&query=horse&start=&rows=1&format=netpunkt_standard&trackingId=test200", dropWizzardRule.getLocalPort()))
+                .request()
+                .get();
+        assertEquals(200, response.getStatus());
+    }
+}
